352cdccff10e605fde70c64cc4cf48332488ecc6
Terax <19204073+terax6669@users.noreply.github.com>
2021-07-30 20:49:45 +0200

    Update

--- src/components/Tip.vue
+++ src/components/Tip.vue
@@ -14,16 +14,14 @@
       },
     },
 
-    render(_, context) {
-      const { props, children } = context
-
+    render(oldProps, unknown, props) {
       return (
         <div class={`vue-treeselect__tip vue-treeselect__${props.type}-tip`}>
           <div class="vue-treeselect__icon-container">
             <span class={`vue-treeselect__icon-${props.icon}`} />
           </div>
           <span class={`vue-treeselect__tip-text vue-treeselect__${props.type}-tip-text`}>
-            {children}
+            {this.$slots.default()}
           </span>
         </div>
       )
